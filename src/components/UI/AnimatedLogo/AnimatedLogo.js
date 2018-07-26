0a5dcf19c94119dc87f19ecaab881b5cc1251ee7
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-07-26 09:37:35 +0000

    feat: add canvas to the animated logo

--- src/components/UI/AnimatedLogo/AnimatedLogo.js
+++ src/components/UI/AnimatedLogo/AnimatedLogo.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './AnimatedLogo.css';
 
-const animatedLogo = () => {
-  return (
-    <span className={classes.AnimatedLogo}>
-      <span className={classes.Tile} />
-    </span>
-  );
+class AnimatedLogo extends Component {
+  drawCanvas() {
+    const ctx = this.refs.canvas.getContext('2d');
+    ctx.strokeStyle = '#2a728f';
+    ctx.beginPath();
+    [[10,0,10,30],[20,0,20,30],[0,10,30,10],[0,20,30,20]].forEach((cords) => {
+      ctx.moveTo(cords[0], cords[1]);
+      ctx.lineTo(cords[2], cords[3]);
+    });
+    ctx.stroke();
+  }
+
+  componentDidMount() {
+    this.drawCanvas();
+  }
+
+  render() {
+    return (
+      <span className={classes.AnimatedLogo}>
+        <canvas
+          ref="canvas"
+          id="animatedlogo-background"
+          className={classes.Background}
+          width="30" height="30"
+        ></canvas>
+        <span className={classes.Tile} />
+      </span>
+    );
+  }
 };
 
-export default animatedLogo;
+export default AnimatedLogo;
